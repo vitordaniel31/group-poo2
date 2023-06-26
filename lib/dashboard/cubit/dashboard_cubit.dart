3d063e59802a2e2a5333484fa69b379ff835adb5
ketwy <ketllyazevedo14@gmail.com>
2023-06-26 12:59:09 -0300

    fix: adicionando informações de detalhar

--- lib/dashboard/cubit/dashboard_cubit.dart
+++ lib/dashboard/cubit/dashboard_cubit.dart
@@ -8,20 +8,86 @@ class Character {
     this.id = 0,
     this.name = '',
     this.image = '',
+    this.href = '',
+    this.levels = const [],
+    this.type = '',
+    this.atributos = const [],
+    this.fields = '',
+    this.criacao = '',
+    this.descricao = '',
+    this.skills = const [],
+    this.priorEvolutions = const [],
+    this.nextEvolutions = const [],
   });
 
   factory Character.fromJson(Map<String, dynamic> json) {
+    final levelsData = json['levels'] as List<dynamic>?;
+    final atributosData = json['attributes'] as List<dynamic>?;
+    final skillsData = json['skills'] as List<dynamic>?;
+    final priorData = json['priorEvolutions'] as List<dynamic>?;
+    final nextData = json['nextEvolutions'] as List<dynamic>?;
+
+    // ignore: omit_local_variable_types, prefer_final_locals, lines_longer_than_80_chars
+    List<Map<String, dynamic>> levels =
+        levelsData != null ? List<Map<String, dynamic>>.from(levelsData) : [];
+    // ignore: omit_local_variable_types, prefer_final_locals, lines_longer_than_80_chars
+    List<Map<String, dynamic>> atributos = atributosData != null
+        ? List<Map<String, dynamic>>.from(atributosData)
+        : [];
+    // ignore: omit_local_variable_types, prefer_final_locals, lines_longer_than_80_chars
+    List<Map<String, dynamic>> skills =
+        skillsData != null ? List<Map<String, dynamic>>.from(skillsData) : [];
+    // ignore: omit_local_variable_types, prefer_final_locals, lines_longer_than_80_chars
+    List<Map<String, dynamic>> priorEvolutions =
+        priorData != null ? List<Map<String, dynamic>>.from(priorData) : [];
+    // ignore: omit_local_variable_types, prefer_final_locals, lines_longer_than_80_chars
+    List<Map<String, dynamic>> nextEvolutions =
+        nextData != null ? List<Map<String, dynamic>>.from(nextData) : [];
+
     return Character(
       id: json['id'] as int,
       name: utf8.decode(json['name'].toString().codeUnits),
       image: (json.containsKey('image') ? json['image'] : '') as String,
+      criacao: utf8.decode(json['releaseDate'].toString().codeUnits),
+      href: json.containsKey('images')
+          // ignore: avoid_dynamic_calls
+          ? (json['images'][0]['href'] as String? ?? '')
+          : '',
+      type: json.containsKey('types')
+          // ignore: avoid_dynamic_calls
+          ? (json['types'][0]['type'] as String? ?? '')
+          : '',
+      fields: json.containsKey('fields')
+          // ignore: avoid_dynamic_calls
+          ? (json['fields'][0]['field'] as String? ?? '')
+          : '',
+      descricao: json.containsKey('descriptions')
+          // ignore: avoid_dynamic_calls
+          ? (json['descriptions'][1]['description'] as String? ?? '')
+          : '',
+      levels: levels,
+      atributos: atributos,
+      skills: skills,
+      priorEvolutions: priorEvolutions,
+      nextEvolutions: nextEvolutions,
     );
   }
 
   final int id;
   final String name;
   final String image;
+  final String href;
+  final List<Map<String, dynamic>> levels;
+  final String type;
+  final List<Map<String, dynamic>> atributos;
+  final String fields;
+  final String criacao;
+  final String descricao;
+  final List<Map<String, dynamic>> skills;
+  final List<Map<String, dynamic>> priorEvolutions;
+  final List<Map<String, dynamic>> nextEvolutions;
 }
+
 
 class DashboardCubit extends Cubit<List<Character>> {
   DashboardCubit() : super([]);
