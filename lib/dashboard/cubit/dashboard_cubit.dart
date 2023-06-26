f7b6d254b29ae3d2e9c265ea20943b617c27f0bb
vitor_medeiros <vitordaniel602@gmail.com>
2023-06-26 00:49:08 -0300

    Infinite Scroll

--- lib/dashboard/cubit/dashboard_cubit.dart
+++ lib/dashboard/cubit/dashboard_cubit.dart
@@ -23,39 +23,73 @@ class Character {
   final String image;
 }
 
-List<Character> perOriginal = [];
+List<Character> characters = [];
 
 class DashboardCubit extends Cubit<List<Character>> {
   DashboardCubit() : super([]);
 
+  int pageSize = 20;
+  int currentPage = 1;
+  int totalPages = 1;
+
   Future<void> fetchCharacters(String search) async {
     try {
-      final response = await http.get(
-        Uri.parse('https://www.digi-api.com/api/v1/digimon?pageSize=20'),
-      );
+      if (currentPage <= totalPages) {
+        final response = await http.get(
+          Uri.parse(
+            'https://www.digi-api.com/api/v1/digimon?pageSize=$pageSize&page=$currentPage',
+          ),
+        );
 
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        final characterData = data['content'] as List<dynamic>;
+        if (response.statusCode == 200) {
+          final data = jsonDecode(response.body);
+          totalPages = data['pageable']['totalPages'] as int;
 
-        final fetchedCharacters = characterData
-            .map(
-              (dynamic characterJson) =>
-                  Character.fromJson(characterJson as Map<String, dynamic>),
-            ).where((character) {
-              final searchLowerCase = search.toLowerCase();
-              return searchLowerCase.isEmpty || 
-              (character.id.toString().toLowerCase().contains(searchLowerCase) 
-              || character.name.toLowerCase().contains(searchLowerCase)
-              );
-            },).toList();
+          final characterData = data['content'] as List<dynamic>;
 
-        emit(fetchedCharacters);
+          final fetchedCharacters = characterData
+              .map((dynamic characterJson) =>
+                  Character.fromJson(characterJson as Map<String, dynamic>))
+              .toList();
+
+          if (currentPage == 1) {
+            characters.clear();
+          }
+
+          characters.addAll(fetchedCharacters);
+
+          emit(fetchedCharacters);
+        }
       } else {
         throw Exception('Failed to fetch characters');
       }
     } catch (error) {
-      throw Exception('Failed to fetch champions: $error');
+      throw Exception('Failed to fetch characters: $error');
     }
+  }
+
+  void filterCharacters(String search) {
+    if (search.isEmpty) {
+      emit(characters);
+    } else {
+      final searchLowerCase = search.toLowerCase();
+      final filteredCharacters = characters
+          .where(
+            (character) =>
+                searchLowerCase.isEmpty ||
+                (character.id
+                        .toString()
+                        .toLowerCase()
+                        .contains(searchLowerCase) ||
+                    character.name.toLowerCase().contains(searchLowerCase)),
+          )
+          .toList();
+      emit(filteredCharacters);
+    }
+  }
+
+  void loadMoreCharacters(String search) {
+    currentPage++;
+    fetchCharacters(search);
   }
 }
--- lib/dashboard/view/dashboard_page.dart
+++ lib/dashboard/view/dashboard_page.dart
@@ -22,6 +22,8 @@ class DashboardView extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = context.l10n;
 
+    var currentSearch = '';
+
     return Scaffold(
       appBar: AppBar(title: Text(l10n.dashboardAppBarTitle)),
       body: Column(
@@ -30,7 +32,8 @@ class DashboardView extends StatelessWidget {
             padding: const EdgeInsets.all(16),
             child: TextField(
               onChanged: (search) {
-                context.read<DashboardCubit>().fetchCharacters(search);
+                currentSearch = search;
+                context.read<DashboardCubit>().filterCharacters(search);
               },
               decoration: const InputDecoration(
                 labelText: 'Pesquise por nome ou ID',
@@ -41,45 +44,58 @@ class DashboardView extends StatelessWidget {
             child: BlocBuilder<DashboardCubit, List<Character>>(
               builder: (context, characterList) {
                 if (characterList.isNotEmpty) {
-                  return SingleChildScrollView(
-                    child: DataTable(
-                      dataRowMaxHeight: 100,
-                      columns: const [
-                        DataColumn(label: Text('Nome')),
-                        DataColumn(label: Text('Imagem')),
-                        DataColumn(label: Text('Ações')),
-                      ],
-                      rows: characterList.map((character) {
-                        return DataRow(
-                          cells: [
-                            DataCell(Text(character.name)),
-                            DataCell(
-                              Image.network(
-                              character.image,
-                                height: 80,
-                              ),
-                            ),
-                            DataCell(
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pushNamed(
-                                    context,
-                                    '/detail',
-                                    arguments: {'id': character.id},
-                                  );
-                                },
-                                style: ButtonStyle(
-                                  backgroundColor:
-                                    MaterialStateProperty.all<Color>(Colors.blue),
-                                  foregroundColor:
-                                    MaterialStateProperty.all<Color>(Colors.white),
+                  return NotificationListener<ScrollNotification>(
+                    onNotification: (scrollNotification) {
+                      if (scrollNotification is ScrollEndNotification &&
+                          scrollNotification.metrics.extentAfter == 0) {
+                        context
+                            .read<DashboardCubit>()
+                            .loadMoreCharacters(currentSearch);
+                      }
+                      return false;
+                    },
+                    child: SingleChildScrollView(
+                      child: DataTable(
+                        dataRowMaxHeight: 100,
+                        columns: const [
+                          DataColumn(label: Text('Nome')),
+                          DataColumn(label: Text('Imagem')),
+                          DataColumn(label: Text('Ações')),
+                        ],
+                        rows: characterList.map((character) {
+                          return DataRow(
+                            cells: [
+                              DataCell(Text(character.name)),
+                              DataCell(
+                                Image.network(
+                                  character.image,
+                                  height: 80,
                                 ),
-                                child: const Text('Detalhar'),
                               ),
-                            ),
-                          ],
-                        );
-                      }).toList(),
+                              DataCell(
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pushNamed(
+                                      context,
+                                      '/detail',
+                                      arguments: {'id': character.id},
+                                    );
+                                  },
+                                  style: ButtonStyle(
+                                    backgroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                            Colors.blue),
+                                    foregroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                            Colors.white),
+                                  ),
+                                  child: const Text('Detalhar'),
+                                ),
+                              ),
+                            ],
+                          );
+                        }).toList(),
+                      ),
                     ),
                   );
                 } else {
